1ffffc8f68c24f9f29f63f3fcd5d260f9dd8ec1e
atwaabeah <atwaabeah@gmail.com>
2026-03-03 16:15:31 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+
+#include<iostream>
+using namespace std;
+
+int main() {
+  cout << "HEllo" << "\n";
+  cout << "HEllo" << "\n";
+  cout << "HEllo" << "\n";
+  cout << "HEllo" << "\n";
+  cout << "HEllo" << "\n";
+}
